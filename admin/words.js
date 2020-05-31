ea68bb807b78e9eab3b4f86a94dcbe3b3303dc2a
Dominic Griesel <d.griesel@gmx.net>
2020-05-31 08:03:01 +0200

    fix: add english texts to admin/words.js

--- admin/words.js
+++ admin/words.js
@@ -1,37 +1,48 @@
 ﻿// Dictionary (systemDictionary is global variable from adapter-settings.js)
 systemDictionary = {
 	"G-Homa adapter settings": {
-		"de": "Einstellungen für G-Homa Adapter"
+		"de": "Einstellungen für G-Homa Adapter",
+		"en": "G-Homa adapter settings",
 	},
 	"Settings": {
-		"de": "Einstellungen"
+		"de": "Einstellungen",
+		"en": "Settings"
 	},
 	"Manage devices": {
-		"de": "Geräte verwalten"
+		"de": "Geräte verwalten",
+		"en": "Manage devices"
 	},
 	"WiFi key:": {
-		"de": "WLAN-Passwort:"
+		"de": "WLAN-Passwort:",
+		"en": "WiFi key:"
 	},
 	"This is necessary to include new devices into the network": {
-		"de": "Dies wird benötigt, um neue Geräte ins Netzwerk einzubinden"
+		"de": "Dies wird benötigt, um neue Geräte ins Netzwerk einzubinden",
+		"en": "This is necessary to include new devices into the network",
 	},
 	"add device": {
-		"de": "Gerät hinzufügen"
+		"de": "Gerät hinzufügen",
+		"en": "add device",
 	},
 	"please wait...": {
-		"de": "Bitte warten..."
+		"de": "Bitte warten...",
+		"en": "please wait...",
 	},
 	"Please enter the WiFi key!": {
-		"de": "Bitte den WLAN-Schlüssel eingeben!"
+		"de": "Bitte den WLAN-Schlüssel eingeben!",
+		"en": "Please enter the WiFi key!",
 	},
 	"Network Interface:": {
-		"de": "Netzwerkschnittstelle:"
+		"de": "Netzwerkschnittstelle:",
+		"en": "Network Interface:",
 	},
 	"If you have multiple network interfaces, you can select the correct one": {
-		"de": "Wenn mehrere Netzwerkschnittstellen vorhanden sind, hier die richtige auswählen"
+		"de": "Wenn mehrere Netzwerkschnittstellen vorhanden sind, hier die richtige auswählen",
+		"en": "If you have multiple network interfaces, you can select the correct one",
 	},
 	"on save adapter restarts with new config immediately": {
 		"de": "Beim Speichern von Einstellungen wird der Adapter sofort neu gestartet.",
+		"en": "on save adapter restarts with new config immediately",
 		"ru": "Ñðàçó ïîñëå ñîõðàíåíèÿ íàñòðîåê äðàéâåð ïåðåçàïóñòèòüñÿ ñ íîâûìè çíà÷åíèÿìè"
 	}
 };
